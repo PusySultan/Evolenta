0ad4d74aa992a18610d9d258e8274e3d55a02957
DESKTOP-KLGCVKK\Kirill <kirillsoloduhin072@gmail.com>
2025-03-15 14:15:42 +0300

    add 3 task

---
+++ Task_3/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.io.Console;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int a = 0,
+                b = 0,
+                c = 0;
+
+        System.out.println("Введите 3 числа");
+        a = Reader.SetValue(a);
+        b = Reader.SetValue(b);
+        c = Reader.SetValue(c);
+
+        Reader.ConsoleOutput(a, b, c);
+
+    }
+}
